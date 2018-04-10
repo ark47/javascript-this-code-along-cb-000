22f7e38d766246c52b6cef36153b7ef305b07533
Chris Nunez <cenunez47@gmail.com>
2018-04-10 19:08:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function Sandwich(bread, ingredients, name) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.name = name;
+  this.describe = function() {
+    console.log("Your " + this.name + " includes: " + this.ingredients.join(", ") + ". Yum!");
+  };
+}
+
+function serve() {
+    if(arguments.length > 0) {
+        var customers = Array.prototype.slice.call(arguments);
+        last = customers.pop();
+        console.log(this.name + " for " + customers.join(", ") + " and " + last + ". Enjoy!");
+    }else {
+        console.log(this.name + ". Order up!");
+    }
+}
+ 
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
+var gc = new Sandwich("white", ["cheese"], "Grilled Cheese");
+var pbj = new Sandwich("wheat", ["peanut butter", "raspberry jam"], "Peanut Butter & Jelly");
+
+var salad = {
+  ingredients: ["croutons", "romaine hearts", "steak", "parmesan", "caesar dressing"],
+  name: "Steak Caesar"
+};
+
+function deliverFood(customer, table) {
+  console.log("Delivering " + this.name + " to " + customer + " at table " + table + ".");
+}
+
+function visitTable() {
+  console.log("The server is visiting " + this.name + " at table number " + this.tableNumber);
+}
+
+function Customer(name, tableNumber) {
+  this.name = name;
+  this.tableNumber = tableNumber;
+}
+
+// .call()
+pbj.describe.call(salad);
+
+// .apply()
+serve.apply(pbj, ["Bear", "Tim", "Steve"]);
+
+// .bind()
+salad.describe = pbj.describe.bind(salad);
+salad.describe();
+
+var sally = new Customer("Sally", "4");
+var visitSally = visitTable.bind(sally);
+setTimeout(visitSally, 5000);
